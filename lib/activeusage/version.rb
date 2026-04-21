6bc57478d2444bc841a132b177e1e9751d6056ab
Tomasz Kowalewski <me@tkowalewski.pl>
2026-04-21 22:19:24 +0200

    Initial commit

---
+++ lib/activeusage/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Activeusage
+  VERSION = "0.1.0"
+end
---
+++ spec/activeusage_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Activeusage do
+  it "has a version number" do
+    expect(Activeusage::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
