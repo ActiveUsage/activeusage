7bc9ad89a73a3957249afd9239d1b0cf7502b908
Tomasz Kowalewski <me@tkowalewski.pl>
2026-04-22 21:19:30 +0200

    Remove fake example from ActiveUsage spec

--- spec/active_usage_spec.rb
+++ spec/active_usage_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe ActiveUsage do
   it "has a version number" do
     expect(ActiveUsage::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
