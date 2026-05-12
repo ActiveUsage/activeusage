e71d784fcd60b3ee6732d138b8a3a6fe833d408b
Tomasz Kowalewski <me@tkowalewski.pl>
2026-05-12 18:17:12 +0200

    Restore SQL query tracking in Tracker via scoped RuntimeState subscription

--- lib/active_usage/tracker.rb
+++ lib/active_usage/tracker.rb
@@ -8,28 +8,37 @@ module ActiveUsage
       @started_at = Time.current
     end
 
-    def call
-      result = yield
-
+    def call(&)
+      Instrumentation::RuntimeState.clear_sql_state
+      result = ActiveSupport::Notifications.subscribed(sql_listener, "sql.active_record", &)
       ActiveUsage.record(**attributes)
-
       result
+    ensure
+      Instrumentation::RuntimeState.clear_sql_state
     end
 
     private
+
+    def sql_listener
+      lambda do |_name, started, finished, _id, payload|
+        next if payload[:cached]
+
+        Instrumentation::RuntimeState.add_sql_event(
+          payload,
+          duration_ms: ((finished - started) * 1000.0).round(3)
+        )
+      end
+    end
 
     def attributes
       {
         type: :task,
         name: @name,
         started_at: @started_at,
-        finished_at: finished_at,
-        tags: @tags
+        finished_at: Time.current,
+        tags: @tags,
+        sql_queries: Instrumentation::RuntimeState.consume_sql_queries
       }
-    end
-
-    def finished_at
-      @finished_at ||= Time.current
     end
   end
 end
